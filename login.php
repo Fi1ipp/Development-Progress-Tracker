e1e32a3d8d168d9aa4fcde8d4924cf9e213d61fe
Fi1ipp <167741273+Fi1ipp@users.noreply.github.com>
2026-04-07 12:21:02 +0200

    login-form

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <form action="welcome.php" method="post">
+        Name:   <input type="text" name="name" required placeholder="John Doe"><br><br>
+        E-mail: <input type="text" name="email" required placeholder="john.doe@email.com"><br><br>
+        <input type="submit" id="submit">
+    </form>
+</body>
+</html>
