15abcbc379132bdc45c09366b9761ad93ec93d14
Fi1ipp <167741273+Fi1ipp@users.noreply.github.com>
2026-04-07 11:37:51 +0200

    header

--- index.php
+++ index.php
@@ -4,8 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
+    <header>
+        <div>
+          <a href="index.html">
+            <img src="img/logo.png" height="40">
+          </a>
+        </div>
+      <nav>
+        <ul class="nav-element">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="login.php">Sign In</a></li>
+        </ul>
+      </nav>
+    </header>
     
 </body>
 </html>
