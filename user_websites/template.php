760ca925a55ecda6014e8114e2a93ef37d4647a4
Fi1ipp <167741273+Fi1ipp@users.noreply.github.com>
2026-06-12 00:19:04 +0200

    usercheck-fix

--- user_websites/template.php
+++ user_websites/template.php
@@ -42,7 +42,7 @@ require_once(__ROOT__.'/db/site.php');
 
 
 
-            if ($ids[0] === $_SESSION['user_id']) {
+            if ($ids[0] == $_SESSION['user_id']) {
 
                 echo 'const trackers = new Trackers(localStorage.getItem("'.basename($_SERVER['PHP_SELF'], '.php').'") , JSON.stringify(' . $data . '));';
                 echo 'document.getElementById("container").appendChild(trackers.loadTrackerBase());';
